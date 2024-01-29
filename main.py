cf6836b1908157b91e5f30c7626580258b2a162d
GavinTryz <gavintryz@gmail.com>
2024-01-29 18:41:18 -0500

    openai test

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import random
-import time
+from openai import OpenAI
 
-st.title("Simple chat")
+st.title("Gavin Bot")
+
+client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
+
+if "openai_model" not in st.session_state:
+    st.session_state['openai_model'] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -12,24 +16,23 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        assistant_response = random.choice(
-            [
-                "Hello there! How can I assist you today?",
-                "Hi, human! Is there anything I can help you with?",
-                "Do you need help?"
-            ]
-        )
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(.05)
-            message_placeholder.markdown(full_response + "| ")
+
+
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role":"assistant", "content": full_response})
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
 
